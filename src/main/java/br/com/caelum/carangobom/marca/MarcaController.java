358bc0619da653f414fa95a08ab0f82f3a33d8c1
Diego Lopes <diego.lopes@thoughtworks.com>
2021-07-13 15:28:52 -0300

    Adição do swagger

--- src/main/java/br/com/caelum/carangobom/marca/MarcaController.java
+++ src/main/java/br/com/caelum/carangobom/marca/MarcaController.java
@@ -3,6 +3,8 @@ package br.com.caelum.carangobom.marca;
 import br.com.caelum.carangobom.mappers.MapStructMapper;
 import br.com.caelum.carangobom.marca.dtos.MarcaDto;
 import br.com.caelum.carangobom.marca.entities.Marca;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
@@ -32,17 +34,20 @@ public class MarcaController {
     }
 
     @GetMapping()
+    @ApiOperation("Recupera uma lista de marcas")
     public ResponseEntity<List<MarcaDto>> lista() {
         return ResponseEntity.ok().body(mapper.marcasToMarcasDtos(repository.findAll()));
     }
 
     @GetMapping("/{id}")
+    @ApiOperation("Recupera uma única marca")
     public ResponseEntity<MarcaDto> recuperarMarca(@PathVariable Long id) {
         Optional<Marca> m1 = repository.findById(id);
         return m1.map(marca -> ResponseEntity.ok(mapper.marcaToMarcaDto(marca))).orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping()
+    @ApiOperation("Cadastra uma nova marca")
     public ResponseEntity<MarcaDto> cadastrarMarca(@Valid @RequestBody MarcaDto request, UriComponentsBuilder uriBuilder) {
         Marca marca = repository.save(mapper.marcaDtoToMarca(request));
         URI h = uriBuilder.path("/marcas/{id}").buildAndExpand(request.getId()).toUri();
@@ -50,6 +55,7 @@ public class MarcaController {
     }
 
     @PutMapping("{id}")
+    @ApiOperation("Atualiza uma marca")
     public ResponseEntity<MarcaDto> alterarNomeMarca(@PathVariable Long id, @Valid @RequestBody MarcaDto request) {
         Optional<Marca> marca = repository.findById(id);
         if (marca.isPresent()) {
@@ -61,6 +67,7 @@ public class MarcaController {
     }
 
     @DeleteMapping("/{id}")
+    @ApiOperation("Remove uma marca")
     public ResponseEntity<MarcaDto> deleta(@PathVariable Long id) {
         Optional<Marca> marca = repository.findById(id);
         if (marca.isPresent()) {
--- src/main/java/br/com/caelum/carangobom/marca/dtos/MarcaDto.java
+++ src/main/java/br/com/caelum/carangobom/marca/dtos/MarcaDto.java
@@ -1,19 +1,33 @@
 package br.com.caelum.carangobom.marca.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Data
 @AllArgsConstructor
+@ApiModel(
+        description = "Objeto que representa uma marca"
+)
 public class MarcaDto {
     @JsonProperty("id")
+    @ApiModelProperty(
+            notes = "ID único da marca"
+    )
     private Long id;
 
     @JsonProperty("nome")
     @NotBlank
     @Size(min = 2, message = "Deve ter {min} ou mais caracteres.")
+    @ApiModelProperty(
+            notes = "Nome da marca",
+            example = "Toyota"
+    )
     private String nome;
 }
