6f7ec0a17dc277f7cc81919caf6b95bb9e125ff8
Diego Lopes <diego.lopes@thoughtworks.com>
2021-07-17 14:45:08 -0300

    ajustes no teste de dashboard quebrado #5

--- src/test/java/br/com/caelum/carangobom/services/DashboardServiceTest.java
+++ src/test/java/br/com/caelum/carangobom/services/DashboardServiceTest.java
@@ -1,13 +1,20 @@
 package br.com.caelum.carangobom.services;
 
 import br.com.caelum.carangobom.dtos.DashboardDto;
+import br.com.caelum.carangobom.dtos.VeiculoDto;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+import static org.mockito.MockitoAnnotations.openMocks;
 
 class DashboardServiceTest {
 
@@ -17,8 +24,12 @@ class DashboardServiceTest {
 
     DashboardService service;
 
+    @SuppressWarnings("unchecked")
+    Page<VeiculoDto> veiculos = mock(Page.class);
+
     @BeforeEach
     void setup() {
+        openMocks(this);
         service = new DashboardService(
                 veiculoService
         );
@@ -26,6 +37,7 @@ class DashboardServiceTest {
 
     @Test
     void deveRecuperarDashboard() {
+        when(veiculoService.buscarTodos(any(Pageable.class))).thenReturn(veiculos);
         List<DashboardDto> res = service.recuperarDashboard();
         assertNotNull(res);
     }
