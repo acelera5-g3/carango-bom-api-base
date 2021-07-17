d7efac2a1abce665cc83e1d4f2060b1056ee1372
Diego Lopes <diego.lopes@thoughtworks.com>
2021-07-17 17:23:41 -0300

    Ajustes para autorização no swagger

--- src/main/java/br/com/caelum/carangobom/config/SwaggerConfig.java
+++ src/main/java/br/com/caelum/carangobom/config/SwaggerConfig.java
@@ -3,33 +3,58 @@ package br.com.caelum.carangobom.config;
 import br.com.caelum.carangobom.entities.Usuario;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.schema.ModelRef;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
     @Bean
     public Docket apiDocket() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .securityContexts(Collections.singletonList(securityContext()))
+                .securitySchemes(Collections.singletonList(apiKey()))
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("br.com.caelum.carangobom"))
                 .paths(PathSelectors.any())
                 .build()
-                .ignoredParameterTypes(Usuario.class)
-                .globalOperationParameters(Collections.singletonList(
-                        new ParameterBuilder()
-                                .name("Authorization")
-                                .description("Header para token JWT")
-                                .modelRef(new ModelRef("string"))
-                                .parameterType("header")
-                                .required(false)
-                                .build()
-                ));
+                .ignoredParameterTypes(Usuario.class);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo(
+                "CARANGO BOM API",
+                "Documentação da API da Carango Bom",
+                "1.0",
+                null,
+                null,
+                null,
+                null,
+                Collections.emptyList());
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("JWT", "Bearer", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Collections.singletonList(new SecurityReference("JWT", authorizationScopes));
     }
 }
