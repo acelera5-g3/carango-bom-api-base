e362fc6755c9ef56b2d047c2d25ac9ffb38e13eb
Diego Lopes <diego.lopes@thoughtworks.com>
2021-07-23 13:59:32 -0300

    Ajustes para deploy no heroku referente ao CORS

--- src/main/java/br/com/caelum/carangobom/config/CorsConfig.java
+++ src/main/java/br/com/caelum/carangobom/config/CorsConfig.java
@@ -1,15 +1,18 @@
 package br.com.caelum.carangobom.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig  implements WebMvcConfigurer {
+    @Value("${carangobom.front.url}")
+    private String front;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins(front)
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
